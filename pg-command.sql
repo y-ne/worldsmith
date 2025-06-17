ef377ef460bcdb49c8030aa7d2ac31a4df7b1302
Kuu <73810807+y-ne@users.noreply.github.com>
2025-06-17 17:45:14 +0700

    Update pg-command.sql

--- pg-command.sql
+++ pg-command.sql
@@ -6,3 +6,16 @@ SELECT pg_terminate_backend(pid)
 FROM pg_stat_activity
 WHERE pid <> pg_backend_pid()
 AND datname = 'your_database_name';
+
+-- information schema
+SELECT 
+    column_name,
+    data_type,
+    is_nullable,
+    column_default,
+    character_maximum_length,
+    numeric_precision,
+    numeric_scale
+FROM information_schema.columns 
+WHERE table_name = 'your_table_name'
+ORDER BY ordinal_position;
